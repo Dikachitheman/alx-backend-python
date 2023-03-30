772ba3bdfa4eea968ea67deed87df448564de3e8
Dikachitheman <104804948+Dikachitheman@users.noreply.github.com>
2023-03-30 09:54:15 +0100

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Practicing basic type annotations with Python
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function to add two float parameters and return result as float
+    """
+    return a + b
