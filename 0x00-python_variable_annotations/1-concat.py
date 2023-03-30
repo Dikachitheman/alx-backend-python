cb01936a9e18fe4bbef34bd177bd78140a895de4
Dikachitheman <104804948+Dikachitheman@users.noreply.github.com>
2023-03-30 10:00:46 +0100

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic type annotations - Concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns a concatenated string by joining str1 and str2
+    """
+    return "{}{}".format(str1, str2)
